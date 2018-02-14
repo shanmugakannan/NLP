a51e1d64970b06e0bac4080746b1504b2fcc8dad
Shanmugakannan(snatarajapillai) <snatarajapillai@paypal.com>
2018-02-14 10:29:04 +0530

    Web interface

--- similar.py
+++
@@ -1,7 +0,0 @@
-import nltk
-from nltk.corpus import wordnet
-# Let's compare the noun of "ship" and "boat:"
- 
-w1 = wordnet.synset('delete.v.01') # v here denotes the tag verb
-w2 = wordnet.synset('remove.v.01')
-print(w1.wup_similarity(w2))
